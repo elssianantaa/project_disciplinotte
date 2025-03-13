e2051e58a9b54245a83417949dffdba10bab1a3f
elssianantaa <elsianantaa12@gmail.com>
2025-03-13 08:23:16 +0700

    template

---
+++ app/Http/Controllers/dasboardController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class dasboardController extends Controller
+{
+    //
+    public function showDasboard(){
+        return view('admin.index');
+    }
+}
